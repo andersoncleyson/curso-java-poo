e09a8987bc1dc6563fc8f4fe1350627c8c183804
elliot <andersoncleyson@protonmail.com>
2025-01-21 21:21:50 -0300

    exercícios pt1

--- src/introducaoPOO/test/ProgramTest.java
+++ src/introducaoPOO/test/ProgramTest.java
@@ -2,30 +2,34 @@ package introducaoPOO.test;
 
 import introducaoPOO.domain.AreaTriangulo;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ProgramTest {
     public static void main(String[] args) {
-        double aX, bX, cX, aY, bY, cY;
 
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        AreaTriangulo x, y;
+        x = new AreaTriangulo();
+        y = new AreaTriangulo();
+
         System.out.println("Digite os valores de x");
-        aX = sc.nextDouble();
-        bX = sc.nextDouble();
-        cX = sc.nextDouble();
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
         System.out.println("Digite os valores de y");
-        aY = sc.nextDouble();
-        bY = sc.nextDouble();
-        cY = sc.nextDouble();
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
 
-        AreaTriangulo calculoAreaX = new AreaTriangulo(aX, bX, cX);
-        AreaTriangulo calculoAreaY = new AreaTriangulo(aY, bY, cY);
+        double areaX = x.calculaArea();
+        double areaY = y.calculaArea();
 
-        double areaX = calculoAreaX.calculoArea();
-        double areaY = calculoAreaY.calculoArea();
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n", areaY);
 
-        System.out.println(areaX);
-        System.out.println(areaY);
+        sc.close();
 
     }
 }
