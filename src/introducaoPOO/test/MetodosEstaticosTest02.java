f730d7d9356dbaf32199632312b36779e65363b2
elliot <andersoncleyson@protonmail.com>
2025-01-24 10:31:52 -0300

    Membros estáticos

---
+++ src/introducaoPOO/test/MetodosEstaticosTest02.java
@@ -0,0 +1,29 @@
+package introducaoPOO.test;
+
+import introducaoPOO.domain.Calculator;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MetodosEstaticosTest02 {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter radius: ");
+
+        double radius = sc.nextDouble();
+
+        double c = Calculator.circumference(radius);
+
+        double v = Calculator.volume(radius);
+
+        System.out.printf("Circumference: %.2f%n", c);
+        System.out.printf("Circumference: %.2f%n", v);
+        System.out.printf("PI value: %.2f%n", Calculator.PI);
+
+        sc.close();
+    }
+
+}
