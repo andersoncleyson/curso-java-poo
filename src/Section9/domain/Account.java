f56726b1f4d05f09e161c120d850eb627153f979
Elliot Anderson <andersoncleyson@protonmail.com>
2025-01-28 18:52:53 -0300

    Exercicio Section 9

---
+++ src/Section9/domain/Account.java
@@ -0,0 +1,50 @@
+package Section9.domain;
+
+public class Account {
+    private static final double TAX = 5.00;
+    private int accountNumber;
+    private String accountHolder;
+    private double balance;
+
+    public Account(int accountNumber, String accountHolder) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+    }
+
+    public Account(int accountNumber, String accountHolder, double balance){
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double value) {
+        this.balance += value;
+    }
+
+    public void withdraw(double value) {
+        this.balance -= value + TAX;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account " + this.accountNumber + ", "
+                + "Holder: " + this.accountHolder +
+                ", Balance: $ " + String.format("%.2f", this.balance);
+    }
+}
---
+++ src/Section9/test/AccountTest.java
@@ -0,0 +1,50 @@
+package Section9.test;
+
+import Section9.domain.Account;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AccountTest {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter account number: ");
+        int accountNumber = sc.nextInt();
+
+        System.out.printf("Enter account holder: ");
+        sc.nextLine();
+        String accountHolder = sc.nextLine();
+
+        System.out.print("Is there an initial deposist (y/n)? ");
+        String answer = sc.next();
+        char choice = answer.charAt(0);
+
+        double depositValue;
+
+        Account account;
+
+        if (choice == 'y') {
+            System.out.printf("Enter initial deposit value: ");
+            depositValue = sc.nextDouble();
+            account = new Account(accountNumber, accountHolder, depositValue);
+        } else {
+            account = new Account(accountNumber, accountHolder);
+        }
+        System.out.println("Account data: ");
+        System.out.println(account);
+
+        System.out.println();
+        System.out.print("Enter a deposit value: ");
+        depositValue = sc.nextDouble();
+        account.deposit(depositValue);
+        System.out.println(account);
+
+        System.out.println();
+        System.out.print("Enter a withdraw value: ");
+        double withDraw = sc.nextDouble();
+        account.withdraw(withDraw);
+        System.out.println(account);
+    }
+}
