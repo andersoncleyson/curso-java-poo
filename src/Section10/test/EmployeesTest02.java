667aa7da065e562eaf96a2e3eec56479947ef06a
Anderson Cleyson Soares e Silva <andersoncleyson@inorpelcybersecurity.com.br>
2025-02-13 17:27:17 -0300

    Exercicio corrigido

--- src/Section10/test/EmployeesTest02.java
+++ src/Section10/test/EmployeesTest02.java
@@ -17,17 +17,19 @@ public class EmployeesTest02 {
 
         System.out.printf("How many employees will be registered? ");
         int N = sc.nextInt();
+        sc.nextLine();
 
         for (int i=0; i<N; i++){
             System.out.println();
-            System.out.printf("Emplyoee #" + (i + 1) + ":");
+            System.out.printf("Emplyoee #" + (i + 1) + ":\n");
             System.out.printf("Id: ");
             Integer id = sc.nextInt();
             sc.nextLine();
             System.out.printf("Name: ");
             String name = sc.nextLine();
-            System.out.printf("Salary");
+            System.out.printf("Salary: ");
             Double salary = sc.nextDouble();
+            sc.nextLine();
 
             Employees emp = new Employees(id, name, salary);
 
@@ -37,11 +39,25 @@ public class EmployeesTest02 {
 
         System.out.print("Enter the employee id what will have salary increase: ");
         int idsalary = sc.nextInt();
+        Integer pos = position(list, idsalary);
+        if (pos == null) {
+            System.out.println("This id does not exist");
+        } else {
+            System.out.print("Enter percentage: ");
+            double percent = sc.nextDouble();
+            list.get(pos).increaseSalary(percent);
+        }
+
+        System.out.println();
+        System.out.println("List of employees:");
+        for(Employees employee: list) {
+            System.out.println(employee);
+        }
 
         sc.close();
     }
 
-    public Integer position(List<Employees> list, int id) {
+    public static Integer position(List<Employees> list, int id) {
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).getId() == id) {
                 return i;
